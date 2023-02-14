d64a086d4639eba7a12c77bd1b7a0582baaf9960
Алексей Цыганков <almeonamy@gmail.com>
2023-02-15 01:07:05 +0700

    Добавьте файлы проекта.

---
+++ LibreOfficeTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using LibreOfficeLibrary;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+var converter = new DocumentConverter();
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+const int count = 100;
+
+for (int i = 0; i < count; i++)
+{
+    converter.ConvertToPdf("../../../input.docx", $"../../../output{i}.pdf");
+}
+
+sw.Stop();
+
+Console.WriteLine($"Done! Time per file: {sw.Elapsed.TotalSeconds / count} s");
